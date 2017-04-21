6f3ec551716d92bf3d217550ecd83c67734d4809
unknown <daniel.bermudez@gmail.com>
2017-04-21 10:21:43 +0200

    p

--- src/app/factories/animals-factory.js
+++ src/app/factories/animals-factory.js
@@ -7,28 +7,14 @@
         var module = {};
         var self = module;
 
-        var allAnimals = [
-            {
-                "specie": "Labrador",
-                "url": "https://es.wikipedia.org/wiki/Labrador_retriever",
-                "origen":"Canadá"
-            },
-            {
-                "specie": "Pastor Alemán",
-                "url": "https://es.wikipedia.org/wiki/Pastor_alem%C3%A1n",
-                "origen":"Aleman"
-            },
-            {
-                "specie": "Bulldog",
-                "url": "https://es.wikipedia.org/wiki/Bulldog",
-                "origen":"Reino Unido"
-            },
-            {
-                "specie": "Beagle",
-                "url": "https://es.wikipedia.org/wiki/Beagle",
-                "origen":"Reino Unido"
-            }
-        ];
+    var allAnimals = [
+        {id: 0, name: 'Gatos', img:'cats.jpg'},
+        {id: 1, name: 'Camaleones', img:'camaleones.jpg'},
+        {id: 2, name: 'Perros', img: 'dogs.jpg'},
+        {id: 3, name: 'Hipopótamos', img: 'hipo.jpg'},
+        {id: 4, name: 'Cigüeñas', img: 'ciguenas.jpg'},
+        {id: 5, name: 'Pulpos', img: 'pulpo.jpg'},
+    ];
         
         module.getAllAnimals = function () {
             return allAnimals;
