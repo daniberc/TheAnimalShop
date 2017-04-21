d91600bd499091f5d21003c39cc28d139a42a80b
unknown <daniel.bermudez@gmail.com>
2017-04-21 09:56:52 +0200

    Detail

--- src/app/components/viewDetail/view-detail.js
+++ src/app/components/viewDetail/view-detail.js
@@ -16,7 +16,7 @@
     /* Este método equivale al $( document ).ready() de jQuery.
     En el $onInit seteamos las propiedades de este objeto-controlador (vm, que es this, que es este componente), que le vamos a pasar a la template */
     vm.$onInit = function() {
-      vm.foo = 'Soy una propiedad';
+      vm.foo = 'Soy el detalle';
     }
   }
   })(angular);
